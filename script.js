88b40cd292535c2edafa5aa17543e3d651524b41
Cassio Felippe <cassioluis12@gmail.com>
2020-06-22 21:54:28 -0300

    Added drag n drop handler

--- script.js
+++ script.js
@@ -1,55 +1,71 @@
-const url = `https://open.spotify.com/`;
+window.onload = function() {
+  $(document).ready(function() {
+    let dragger = tableDragger(document.querySelector('table#ranking'), {
+      mode: 'row',
+      dragHandler: '.handler',
+      onlyBody: true
+    });
 
-request = function({route = 'track', id, body, method = 'GET', callback = function(){}} = {}) {
-  let request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if (this.readyState == 4) {
-      if (this.status == 200) {
-        return callback(JSON.parse(request.responseText)['response']);
-      } else if (![200].includes(this.status)) {
-        alert('Request failed!');
+    dragger.on('drop',function(from, to){
+      console.log(from);
+      console.log(to);
+    });
+
+    const url = `https://open.spotify.com/`;
+
+    request = function({route = 'track', id, body, method = 'GET', callback = function(){}} = {}) {
+      let request = new XMLHttpRequest();
+      request.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            return callback(JSON.parse(request.responseText)['response']);
+          } else if (![200].includes(this.status)) {
+            alert('Request failed!');
+          }
+        }
+      };
+
+      request.open(method, `${url}/${route}/${id}`);
+      request.send();
+    };
+
+    getTrack = function() {
+      request({route: 'track', id: '0kbhaFa3siTlOAwtPc3NcB'});
+    };
+
+    create = function(tag) {
+      return document.createElement(tag);
+    };
+
+    createTr = function({route, text, action, body, method, callback} = {}) {
+      let tr = create('tr');
+      let button = create('button');
+
+      action = this[action];
+
+      button.onclick = function() {
+        return action({route, body, method, callback});
       }
-    }
-  };
+      button.innerText = text;
+      tr.appendChild(button);
 
-  request.open(method, `${url}/${route}/${id}`);
-  request.send();
-};
+      return tr;
+    };
 
-getTrack = function() {
-  request({route: 'track', id: '0kbhaFa3siTlOAwtPc3NcB'});
-};
+    createTables = function({item, tbody} = {}) {
+      for (const key in item) {
+        let tr = create('tr');
 
-create = function(tag) {
-  return document.createElement(tag);
-};
+        let tdKey = create('td');
+        tdKey.appendChild(document.createTextNode(key));
+        tr.appendChild(tdKey);
+        let tdValue = create('td');
+        tdValue.appendChild(document.createTextNode(item[key]));
+        tr.appendChild(tdValue);
+        
+        tbody.appendChild(tr);
+      }
+    };
+  });
+}
 
-createTr = function({route, text, action, body, method, callback} = {}) {
-  let tr = create('tr');
-  let button = create('button');
-
-  action = this[action];
-
-  button.onclick = function() {
-    return action({route, body, method, callback});
-  }
-  button.innerText = text;
-  tr.appendChild(button);
-
-  return tr;
-};
-
-createTables = function({item, tbody} = {}) {
-  for (const key in item) {
-    let tr = create('tr');
-
-    let tdKey = create('td');
-    tdKey.appendChild(document.createTextNode(key));
-    tr.appendChild(tdKey);
-    let tdValue = create('td');
-    tdValue.appendChild(document.createTextNode(item[key]));
-    tr.appendChild(tdValue);
-    
-    tbody.appendChild(tr);
-  }
-};
